6c5d3e9e941aec63bf072273ae4d0fef43d860ab
Fangyi Chen <FAC35@pitt.edu>
2022-12-07 04:23:54 -0500

    querynum

--- configs/sqr/adamixer_Qrecycle_r50_1x_coco.py
+++ configs/sqr/adamixer_Qrecycle_r50_1x_coco.py
@@ -18,7 +18,7 @@ IMAGE_SCALE = (1333, 800)
 
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = '/home/fangyi/data/COCO/'
+data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
