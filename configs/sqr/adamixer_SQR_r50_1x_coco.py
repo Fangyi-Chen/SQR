21e5e90e1c8a46278f18ad92ac6739a0757c81b9
Fangyi <FAC35@pitt.edu>
2022-12-14 19:04:30 -0500

    refactor

--- configs/sqr/adamixer_SQR_r50_1x_coco.py
+++ configs/sqr/adamixer_SQR_r50_1x_coco.py
@@ -12,7 +12,7 @@ _base_ = [
     '../_base_/datasets/coco_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
-work_dir_prefix = 'work_dirs/adamixer_mmdet'
+work_dir_prefix = 'work_dirs/SQR-adamixer-r50'
 
 IMAGE_SCALE = (1333, 800)
 
